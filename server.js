1eeea3a0ec6b665466e00194fbd531cda9a2c876
Ryu_woong <jin02014@naver.com>
2023-02-16 19:53:30 +0900

    회원이 아닐경우 게시글 못만드는 것 간이로 만들어봄
    react를 배우고 다시 graphql로 돌아와서 만들어 볼 예정! 다 합쳐서 만들어봅시다~
    끝이 다가온다 화이팅~

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 import { ApolloServer, gql } from "apollo-server";
 
-let fakeDatabase = [
+let Tweets = [
   {
     id: "1",
     text: "첫 번째 사람",
+    author: {
+      id: "2",
+      firstName: "lobert",
+      lastName: "grack",
+    },
   },
   {
     id: "2",
     text: "두 번째 사람",
+    author: {
+      id: "1",
+      firstName: "Woong",
+      lastName: "Ryu",
+    },
+  },
+];
+
+let Users = [
+  {
+    id: "1",
+    firstName: "Woong",
+    lastName: "Ryu",
+  },
+  {
+    id: "2",
+    firstName: "lobert",
+    lastName: "grack",
   },
 ];
 
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
+    firstName: String!
+    lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -25,6 +50,7 @@ const typeDefs = gql`
     #유저가 데이터를 받고 싶다면 이곳에 저장해야 한다.
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    allUsers: [User!]!
   }
   type Mutation {
     #유저가 정보를 보내서 database를 업데이트 하면 이곳에 저장해야한다.
@@ -36,26 +62,48 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     tweet(root, { id }) {
-      return fakeDatabase.find((tweet) => tweet.id === id);
+      return Tweets.find((tweet) => tweet.id === id);
     },
     allTweets() {
-      return fakeDatabase;
+      return Tweets;
+    },
+    allUsers() {
+      return Users;
     },
   },
   Mutation: {
     postTweet(_, { text, userId }) {
-      const newPerson = {
-        id: userId,
-        text: text,
-      };
-      fakeDatabase.push(newPerson);
-      return newPerson;
+      try {
+        if (!Users.find((user) => user.id === userId)) {
+          throw new Error(`회원이 아닙니다`);
+        }
+        const newTweet = {
+          id: Tweets.length + 1,
+          text,
+          userId,
+        };
+        console.log(newTweet);
+        Tweets.push(newTweet);
+        return newTweet;
+      } catch (e) {
+        console.log(e);
+      }
     },
     deleteTweet(_, { id }) {
-      const tweet = fakeDatabase.find((fakeDatabase) => fakeDatabase.id === id);
+      const tweet = Tweets.find((Tweets) => Tweets.id === id);
       if (!tweet) return false;
-      fakeDatabase = fakeDatabase.filter((element) => element.id !== id);
+      Tweets = Tweets.filter((element) => element.id !== id);
       return true;
+    },
+  },
+  User: {
+    fullName(root) {
+      return `${root.firstName} ${root.lastName}`;
+    },
+  },
+  Tweet: {
+    author(root) {
+      return Users.find((tweet) => tweet.id === root.author);
     },
   },
 };
